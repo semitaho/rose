1e8f299148e002279697dc132cb0a51492253793
Toni Aho <toni@tahoo.fi>
2025-10-08 23:16:18 +0300

    ignore

---
+++ colors.js
@@ -0,0 +1,8 @@
+import { Color3 } from "@babylonjs/core";
+
+export const LIGHT_BLUE = new Color3(0.4, 0.7, 1.0);
+export const DEEPER_BLUE = new Color3(0.0, 0.5, 1.0); 
+export const BLACK = new Color3(0, 0, 0);
+export const PINK = new Color3(210 / 255, 151 / 255, 161 / 255);
+export const WING_COLOR =  new Color3(95 / 255, 234 / 255, 249 / 255);
+export const YELLOW = new Color3(1, 1, 0)
